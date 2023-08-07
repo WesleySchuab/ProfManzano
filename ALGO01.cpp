77a0b32a67349738e4b9ab9e10abe0ff732e68b7
Wesley schuab Vieira <58818125+WesleySchuab@users.noreply.github.com>
2023-08-07 19:53:15 -0300

    ola mundo em c++

---
+++ ALGO01.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main (void)
+{
+    cout<<"Alo, mundo!";
+    cout<<endl;
+    cout<<"Tecle <enter> para continuar...";
+    cin.get();
+    return 0;
+}

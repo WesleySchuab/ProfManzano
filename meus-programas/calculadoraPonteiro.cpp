9e868990b3d91505b315bd7a95d1d6ff70be30de
Wesley schuab Vieira <58818125+WesleySchuab@users.noreply.github.com>
2024-06-13 07:29:37 -0300

    primeiro programa com ponteiro

--- meus-programas/calculadoraPonteiro.cpp
+++ meus-programas/calculadoraPonteiro.cpp
@@ -1,8 +1,30 @@
 //Progrma calculadora com ponteiro
 #include<iostream>
 using namespace std;
+
+float soma(float valor1, float valor2)
+{
+        return valor1+valor2 ;
+}
+
 int main (void)
 {
-    int valor1, valor2,
-    int *pvalor1 = 0, pvalor2 = 0;
+    float valor1 = 0 , valor2 = 0;
+    float *pvalor1 = &valor1, *pvalor2 = &valor2;
+
+    cout << "Digite o valor 1 ";
+    cin >> *pvalor1;
+    cin.ignore(80, '\n');
+
+    cout << "Digite o valor 1 ";
+    cin >> *pvalor2;
+    cin.ignore(80, '\n');
+
+    cout << *pvalor1 << " + " << *pvalor2 << " = " << soma(*pvalor1, *pvalor2);
+
+
+
+
+
+
 }
